77728cd741bb41a79b6006e96dcff4b9e0e9f077
MarquinhoCoelho <mvscmarcos@gmail.com>
2025-09-06 14:36:15 -0300

    mudando a porta

--- server.js
+++ server.js
@@ -401,6 +401,7 @@ app.post('/diagnostico', upload.single('audio'), async (req, res) => {
 	}
 });
 
-app.listen(3000, () => {
-	console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3331;
+app.listen(PORT, () => {
+	console.log(`Servidor rodando na porta ${PORT}`);
 });
